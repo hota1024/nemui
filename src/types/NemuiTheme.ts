9574fcfef6689077eeda836a319edcf49358ef38
hota1024 <hotalog@hota1024.com>
2021-06-11 12:13:09 +0900

    :label: add NemuiTheme type

---
+++ src/types/NemuiTheme.ts
@@ -0,0 +1,78 @@
+/**
+ * NemuiThemeType type.
+ */
+export type NemuiThemeType = 'light' | 'dark'
+
+/**
+ * NemuiPalette type.
+ */
+export type NemuiPalette = {
+  /**
+   * main color.
+   */
+  main: string
+
+  /**
+   * dark color.
+   */
+  dark: string
+
+  /**
+   * light color.
+   */
+  light: string
+
+  /**
+   * text color.
+   */
+  text: string
+}
+
+/**
+ * NemuiSpacingFn type.
+ */
+export type NemuiSpacingFn = (factor: number) => string
+
+/**
+ * NemuiColor type.
+ */
+export type NemuiColor =
+  | 'default'
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'info'
+  | 'warning'
+  | 'error'
+
+/**
+ * NemuiTheme type.
+ */
+export type NemuiTheme = {
+  /**
+   * theme type(`light` or `dark`).
+   */
+  type: NemuiThemeType
+
+  /**
+   * background color.
+   */
+  background: string
+
+  /**
+   * foreground color.
+   */
+  foreground: string
+
+  /**
+   * palettes.
+   */
+  palettes: {
+    [K in NemuiColor]: NemuiPalette
+  }
+
+  /**
+   * spacing function.
+   */
+  spacing: NemuiSpacingFn
+}
