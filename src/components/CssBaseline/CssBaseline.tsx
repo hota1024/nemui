4078b46a93ddee061c067d0683e1abe575d582ab
hota1024 <hotalog@hota1024.com>
2021-06-12 00:38:05 +0900

    🐛 fix unit

--- src/components/CssBaseline/CssBaseline.tsx
+++ src/components/CssBaseline/CssBaseline.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 export const CssBaseline = createGlobalStyle`
   html, body {
-    padding: 0;
-    margin: 0;
+    padding: 0px;
+    margin: 0px;
   }
 `
