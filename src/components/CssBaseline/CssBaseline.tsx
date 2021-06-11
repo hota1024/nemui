a267cbae9075df6e2086467409627fc9fb9367b8
hota1024 <hotalog@hota1024.com>
2021-06-12 01:47:05 +0900

    💄 add font

--- src/components/CssBaseline/CssBaseline.tsx
+++ src/components/CssBaseline/CssBaseline.tsx
@@ -4,6 +4,10 @@ export const CssBaseline = createGlobalStyle`
   html, body {
     padding: 0px;
     margin: 0px;
+
+    font-size: 16px;
+    font-family: Roboto,Helvetica,Arial,sans-serif;
+    font-weight: 400;
   }
 
   * {
